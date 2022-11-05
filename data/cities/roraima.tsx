e5b1451bdd71dbfa454d96322f3490ed4ede9beb
Francine Hahn <fran_hahn@hotmail.com>
2022-11-05 16:27:58 -0300

    Adição das cidades de minas até sc

--- data/cities/roraima.tsx
+++ data/cities/roraima.tsx
@@ -1,1 +1,2 @@
-export const roraima = []
+export const roraima = ['Amajari', 'Alto Alegre', 'Boa Vista', 'Bonfim', 'Cantá', 'Caracaraí', 'Caroebe', 'Iracema', 'Mucajaí',
+    'Normandia', 'Pacaraima', 'Rorainópolis', 'São João da Baliza', 'São Luiz', 'Uiramutã']
--- data/cities/santaCatarina.tsx
+++ data/cities/santaCatarina.tsx
@@ -1,1 +1,295 @@
-export const santaCatarina = []
+export const santaCatarina = [Abdon Batista
+    2	Abelardo Luz
+    3	Agrolândia
+    4	Agronômica
+    5	Água Doce
+    6	Águas de Chapecó
+    7	Águas Frias
+    8	Águas Mornas
+    9	Alfredo Wagner
+    10	Alto Bela Vista
+    11	Anchieta
+    12	Angelina
+    13	Anita Garibaldi
+    14	Anitápolis
+    15	Antônio Carlos
+    16	Apiúna
+    17	Arabutã
+    18	Araquari
+    19	Araranguá
+    20	Armazém
+    21	Arroio Trinta
+    22	Arvoredo
+    23	Ascurra
+    24	Atalanta
+    25	Aurora
+    26	Balneário Arroio do Silva
+    27	Balneário Barra do Sul
+    28  Balneário Camboriú
+    29	Balneário Gaivota
+    30	Balneário Piçarras
+    31	Balneário Rincão
+    32	Bandeirante
+    33	Barra Bonita
+    34	Barra Velha
+    35	Bela Vista do Toldo
+    36	Belmonte
+    37	Benedito Novo
+    38	Biguaçu
+    39	Blumenau
+    40	Bocaina do Sul
+    41  Bom Jardim da Serra
+    42	Bom Jesus
+    43	Bom Jesus do Oeste
+    44	Bom Retiro
+    45	Bombinhas
+    46	Botuverá
+    47	Braço do Norte
+    48	Braço do Trombudo
+    49	Brunópolis
+    50	Brusque
+    51	Caçador
+    52	Caibi
+    53	Calmon
+    54	Camboriú
+    55	Campo Alegre
+    56	Campo Belo do Sul
+    57	Campo Erê
+    58	Campos Novos
+    59	Canelinha
+    60	Canoinhas
+    61	Capão Alto
+    62	Capinzal
+    63	Capivari de Baixo
+    64	Catanduvas
+    65	Caxambu do Sul
+    66	Celso Ramos
+    67	Cerro Negro
+    68	Chapadão do Lageado
+    69	Chapecó
+    70	Cocal do Sul
+    71	Concórdia
+    72	Cordilheira Alta
+    73	Coronel Freitas
+    74	Coronel Martins
+    75	Correia Pinto
+    76	Corupá
+    77	Criciúma
+    78	Cunha Porã
+    79	Cunhataí
+    80	Curitibanos
+    81	Descanso
+    82	Dionísio Cerqueira
+    83	Dona Emma
+    84	Doutor Pedrinho
+    85	Entre Rios
+    86	Ermo
+    87	Erval Velho
+    88	Faxinal dos Guedes
+    89	Flor do Sertão
+    90	Florianópolis
+    91	Formosa do Sul
+    92	Forquilhinha
+    93	Fraiburgo
+    94	Frei Rogério
+    95	Galvão
+    96	Garopaba
+    97	Garuva
+    98	Gaspar
+    99	Governador Celso Ramos
+    100	Grão-Pará
+    101	Gravatal
+    102	Guabiruba
+    103	Guaraciaba
+    104	Guaramirim
+    105	Guarujá do Sul
+    106	Guatambu
+    107	Herval dOeste
+    108	Ibiam
+    109	Ibicaré
+    110	Ibirama
+    111	Içara
+    112	Ilhota
+    113	Imaruí
+    114	Imbituba
+    115	Imbuia
+    116	Indaial
+    117	Iomerê
+    118	Ipira
+    119	Iporã do Oeste
+    120	Ipuaçu
+    121	Ipumirim
+    122	Iraceminha
+    123	Irani
+    124	Irati
+    125	Irineópolis
+    126	Itá
+    127	Itaiópolis
+    128	Itajaí
+    129	Itapema
+    130	Itapiranga
+    131 Itapoá
+    132	Ituporanga
+    133	Jaborá
+    134	Jacinto Machado
+    135	Jaguaruna
+    136	Jaraguá do Sul
+    137	Jardinópolis
+    138	Joaçaba
+    139	Joinville
+    140	José Boiteux
+    141	Jupiá
+    142	Lacerdópolis
+    143	Lages
+    144	Laguna
+    145	Lajeado Grande
+    146	Laurentino
+    147	Lauro Müller
+    148	Lebon Régis
+    149	Leoberto Leal
+    150	Lindóia do Sul
+    151	Lontras
+    152	Luiz Alves
+    153	Luzerna
+    154	Macieira
+    155	Mafra
+    156	Major Gercino
+    157	Major Vieira
+    158	Maracajá
+    159	Maravilha
+    160	Marema
+    161	Massaranduba
+    162	Matos Costa
+    163	Meleiro
+    164	Mirim Doce
+    165	Modelo
+    166	Mondaí
+    167	Monte Carlo
+    168	Monte Castelo
+    169	Morro da Fumaça
+    170	Morro Grande
+    171	Navegantes
+    172	Nova Erechim
+    173	Nova Itaberaba
+    174	Nova Trento
+    175	Nova Veneza
+    176	Novo Horizonte
+    177	Orleans
+    178	Otacílio Costa
+    179	Ouro
+    180	Ouro Verde
+    181	Paial
+    182	Painel
+    183	Palhoça
+    184	Palma Sola
+    185	Palmeira
+    186	Palmitos
+    187	Papanduva
+    188	Paraíso
+    189	Passo de Torres
+    190	Passos Maia
+    191	Paulo Lopes
+    192	Pedras Grandes
+    193	Penha
+    194	Peritiba
+    195	Pescaria Brava
+    196	Petrolândia
+    197	Pinhalzinho
+    198	Pinheiro Preto
+    199	Piratuba
+    200	Planalto Alegre
+    201	Pomerode
+    202	Ponte Alta
+    203	Ponte Alta do Norte
+    204	Ponte Serrada
+    205	Porto Belo
+    206	Porto União
+    207	Pouso Redondo
+    208	Praia Grande
+    209	Presidente Castello Branco
+    210	Presidente Getúlio
+    211	Presidente Nereu
+    212	Princesa
+    213	Quilombo
+    214	Rancho Queimado
+    215	Rio das Antas
+    216	Rio do Campo
+    217	Rio do Oeste
+    218	Rio do Sul
+    219	Rio dos Cedros
+    220	Rio Fortuna
+    221	Rio Negrinho
+    222	Rio Rufino
+    223	Riqueza
+    224	Rodeio
+    225	Romelândia
+    226	Salete
+    227	Saltinho
+    228	Salto Veloso
+    229	Sangão
+    230	Santa Cecília
+    231	Santa Helena
+    232	Santa Rosa de Lima
+    233	Santa Rosa do Sul
+    234	Santa Terezinha
+    235	Santa Terezinha do Progresso
+    236	Santiago do Sul
+    237	Santo Amaro da Imperatriz
+    238	São Bento do Sul
+    239	São Bernardino
+    240	São Bonifácio
+    241	São Carlos
+    242	São Cristóvão do Sul
+    243	São Domingos
+    244	São Francisco do Sul
+    245	São João Batista
+    246	São João do Itaperiú
+    247	São João do Oeste
+    248	São João do Sul
+    249	São Joaquim
+    250	São José
+    251	São José do Cedro
+    252	São José do Cerrito
+    253	São Lourenço do Oeste
+    254	São Ludgero
+    255	São Martinho
+    256	São Miguel da Boa Vista
+    257	São Miguel do Oeste
+    258	São Pedro de Alcântara
+    259	Saudades
+    260	Schroeder
+    261	Seara
+    262	Serra Alta
+    263	Siderópolis
+    264	Sombrio
+    265	Sul Brasil
+    266	Taió
+    267	Tangará
+    268	Tigrinhos
+    269	Tijucas
+    270	Timbé do Sul
+    271	Timbó
+    272	Timbó Grande
+    273	Três Barras
+    274	Treviso
+    275	Treze de Maio
+    276	Treze Tílias
+    277	Trombudo Central
+    278	Tubarão
+    279	Tunápolis
+    280	Turvo
+    281	União do Oeste
+    282	Urubici
+    283	Urupema
+    284	Urussanga
+    285	Vargeão
+    286	Vargem
+    287	Vargem Bonita
+    288	Vidal Ramos
+    289	Videira
+    290	Vitor Meireles
+    291	Witmarsum
+    292	Xanxerê
+    293	Xavantina
+    294	Xaxim
+    295 Zortéa]
